2f53c9a90079f92354ab26cc57292194cfcba3f4
Kinyua <alfkinyua@gmail.com>
2022-09-26 14:24:09 +0300

    Update class persons

--- person.rb
+++ person.rb
@@ -1,26 +1,19 @@
 class Person
-
-    def initialize(name="Unknown", age, parent_permission='true')
-        @id =Random.rand (1..1000)
-        @name=name
-        @age=age
-        
+    attr_reader :id, :parent_permission
+    attr_accessor :name, :age
+  
+    def initialize(age, parent_permission = true, name = 'Unknown')
+      @id = Random.rand(1..1000)
+      @name = name
+      @age = age
+      @parent_permission = parent_permission
     end
-end
-   def
-    attr_reader :id
-    attr_reader :name
-    attr_reader :age
-    attr_writer :name 
-    attr_writer :age 
-
-
-   end
-   private
-  def is_of_age
-    if @age >=18 
-        parent_permission=true
-     else
-        parent_permission=true
-     end
+  
+    def is_of_age?
+      @age >= 18
+    end
+  
+    def can_use_services?
+      self.is_of_age? || @parent_permission
+    end
   end
