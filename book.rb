0140348cc6ede78d07ac8389e2b0ae9dbe7b4cc2
Kinyua <alfkinyua@gmail.com>
2022-09-28 14:36:35 +0300

    Populate book with constructor and has_rental method

--- book.rb
+++ book.rb
@@ -1,8 +1,17 @@
 class Book
     attr_accessor :title
     attr_accessor :author
-    def initialize(title,author)
-        @title=title
-        @author=author
+    attr_accessor :rentals
+    def initialize(title, author)
+        @title = title
+        @author = author
+        @rentals = []
+      end
+    
+      def has_rental(rental, person)
+        @rentals.push(rental)
+        rental.book = self
+        rental.person = person
+      end
     end
 end
