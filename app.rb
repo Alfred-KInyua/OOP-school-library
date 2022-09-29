0f554dce3162ceafb9b5d68e1a986e18911a324b
Kinyua <alfkinyua@gmail.com>
2022-09-29 13:48:09 +0300

    Populate book method

--- app.rb
+++ app.rb
@@ -16,36 +16,43 @@ end
 def create_a_person()
   print "Do you want to create a student (1) or a teacher (2)?
   [Input the number]: "
-  answer = gets.chomp
+  ans = gets.chomp
 
-  unless answer == "1"
-    print "Specialization: " 
-    specialization = gets.chomp
-  end
-
-  print "Age: "
+ print "Age: "
   age = gets.chomp
 
   print "Name: "
   name = gets.chomp
 
-  unless answer == "2"
+  if ans == "1"
     print "Has parent permission? [Y/N]: " 
-    parent_permission = gets.chomp.upcase
+    parent_permission = gets.chomp.capitalize
     if parent_permission == "Y"
       parent_permission = true
     elsif parent_permission == "N"
       parent_permission = false
     end
-  end
+    $people << Student.new(age, name, parent_permission)
+elsif answer == "2"
+  print "Specialization: " 
+  specialization = gets.chomp
+  $people << Teacher.new(specialization, age, name)
+end
 
-  $people << Student.new(age, name, parent_permission) unless answer == 2
-  $people << Teacher.new(specialization, age, name) unless answer == 1
 
   puts 'Person created successfully'
 end
 
 def create_a_book()
+    print "Title: "
+    title = gets.chomp
+  
+    print "Author: "
+    author = gets.chomp
+  
+    $books << Book.new(title, author)
+  
+    puts 'Book created successfully'
 end
 
 def create_a_rental()
